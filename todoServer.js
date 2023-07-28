10411570706d4dd4010022f4410a8ec87fd0b34a
Tejas <shimpit45@gmail.com>
2023-07-28 23:27:00 +0530

    This is todo with authentication

--- todoServer.js
+++ todoServer.js
@@ -9,15 +9,44 @@ app.listen(8000, () => {
 });
 
 
+var session = require("express-session"); // Add this line to import express-session
+
+app.use(express.urlencoded({ extended: true }));
+// Configure the express-session middleware
+app.use(
+  session({
+    secret: "keyboard cat",
+    resave: false,
+    saveUninitialized: true,
+    
+  })
+);
+
 app.use(express.json());
 
-app.get('/', function(req, res) {
-    res.sendFile(__dirname + '/todoViews/index.html');
-});
+
 
 app.get('/style.css', function(req, res) {
     res.sendFile(__dirname + '/todoViews/style.css');
 });
+
+app.get("/styles.css", function (req, res) {
+    res.sendFile(__dirname + "/todoViews/styles.css");
+});
+
+app.get("/", function (req, res) {
+    debugger
+    if(!req.session.isLoggedIn)
+    {
+        debugger;
+        res.redirect('/login');
+        return;
+    }
+
+    res.sendFile(__dirname + '/todoViews/index.html')
+  
+});
+
 
 
 app.get('/todoScripts.js', function(req, res) {
@@ -80,3 +109,122 @@ app.post('/update', (req, res) => {
 
 
 
+// logic to login in the site 
+
+app.get("/login", function (req, res) {
+    res.sendFile(__dirname + "/todoViews/login.html");
+  });
+  
+  app.post("/login", function (req, res) {
+    const username = req.body.username;
+    const password = req.body.password;
+  
+    console.log(username, password);
+  
+    fs.readFile('userData.json', 'utf-8', function(err, data){
+      if(err)
+      {
+        console.log(err)
+        return;
+      }
+  
+      const users = JSON.parse(data);
+      const user = users.find(u=> u.username ===username && u.password === password);
+  
+      if (user) {
+        
+        res.cookie('username', user.username);
+        req.session.isLoggedIn = true;
+        return res.redirect('/');
+      } else {
+        return res.redirect('/login');
+      }
+  
+  
+    })
+  
+    // if (username == "t" && password == "t") {
+  
+    //   req.session.isLoggedIn = true;
+    //   req.session.username = username;
+    //   debugger;
+    //   res.redirect("/");
+    //   return;
+    // }
+  
+    // res.status(401).send("error");
+  });
+
+
+  app.get('/logout', (req, res) => {
+    res.isLoggedIn = false;
+    res.clearCookie('username');
+    return res.redirect('/');
+  });
+
+//   Creating the User Account
+
+app.get("/accountCreation", function (req, res) {
+    res.sendFile(__dirname + "/todoViews/accountCreation.html");
+  });
+  
+  app.post('/accountCreation', function(req, res){
+    const name = req.body.name;
+    const username = req.body.username;
+    const password = req.body.password;
+  
+    console.log(" im called")
+    console.log(name, username, password);
+  
+    profile = {
+      name,
+      username,
+      password
+    }
+  
+    fs.readFile('userData.json', 'utf-8', (err, data)=>{
+      if(err)
+      {
+        console.log("Error while reading user file")
+      }
+      
+      const users = JSON.parse(data);
+  
+      const available = users.find(u => u.username === username);
+      if (available) {
+        return res.status(409).send('User with the same username already exists.');
+      }
+  
+      users.push({name, username, password })
+  
+      fs.writeFile('userData.json', JSON.stringify(users), (err)=>{
+        if(err){
+            res.send(profile);
+        }
+  
+        res.redirect('/');
+      })
+  
+    })
+  
+    
+  
+   
+  })
+  
+  app.get("/styles.css", function (req, res) {
+    res.sendFile(__dirname + "/todoViews/styles.css");
+  });
+  
+  app.get("/", function (req, res) {
+      debugger
+      if(!req.session.isLoggedIn)
+      {
+          debugger;
+          res.redirect('/login');
+          return;
+      }
+  
+      res.sendFile(__dirname + '/todoViews/index.html')
+    
+  });
